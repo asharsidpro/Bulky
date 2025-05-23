dafce962202af248f8cb970eb48e81d4e478281f
Hilal Ashhar <asharsid.pro@gmail.com>
2025-05-23 11:41:17 +0530

    add edit and delete button in the index page of category and create the edit page (working) and use update method.

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -37,5 +37,35 @@ namespace BulkyWeb.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id==0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id); //find method works only with primary key.
+            //FirstOrDefault method works in the content of database to find anything including primary key.
+            //u is a kind of loop variable (i.e. in for loop we use i)
+            //this will check the content of id in the database to any field or value.
+            //Category? categoryFromDb2 = _db.Cateogories.FirstOrDefault(u=>u.Id==id);
+            if(categoryFromDb==null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
